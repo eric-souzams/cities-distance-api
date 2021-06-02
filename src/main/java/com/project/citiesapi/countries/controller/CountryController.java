451a4cb0c9d498dcda1197223d6eba5eef3c3b73
Eric Magalhaes <15831837+eric-souzams@users.noreply.github.com>
2021-06-02 12:00:28 -0300

    wip: countries GET endpoint

--- src/main/java/com/project/citiesapi/countries/controller/CountryController.java
+++ src/main/java/com/project/citiesapi/countries/controller/CountryController.java
@@ -2,23 +2,40 @@ package com.project.citiesapi.countries.controller;
 
 import com.project.citiesapi.countries.entities.Country;
 import com.project.citiesapi.countries.repositories.CountryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/countries")
 public class CountryController {
 
-    @Autowired
     private CountryRepository countryRepository;
 
+    public CountryController(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
     @GetMapping
-    public List<Country> getAll() {
-        return countryRepository.findAll();
+    public Page<Country> getAll(Pageable page) {
+        return countryRepository.findAll(page);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Country> getOne(@PathVariable Long id) {
+        Optional<Country> result = countryRepository.findById(id);
+
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(result.get());
     }
 
 }
