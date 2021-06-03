d2fa4699c5fa2c1e04421c85c363d07080a481db
Eric Magalhaes <15831837+eric-souzams@users.noreply.github.com>
2021-06-03 12:28:32 -0300

    wip: get one method on city controller

--- src/main/java/com/project/citiesapi/cities/controller/CityController.java
+++ src/main/java/com/project/citiesapi/cities/controller/CityController.java
@@ -4,23 +4,38 @@ import com.project.citiesapi.cities.entities.City;
 import com.project.citiesapi.cities.repository.CityRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/cities")
 public class CityController {
 
-    private CityRepository repository;
+    private CityRepository cityRepository;
 
-    public CityController(CityRepository repository) {
-        this.repository = repository;
+    public CityController(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
     }
 
     @GetMapping
     public Page<City> cities(Pageable page) {
-        return repository.findAll(page);
+        return cityRepository.findAll(page);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<City> getOne(@PathVariable Long id) {
+        Optional<City> result = cityRepository.findById(id);
+
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(result.get());
     }
 
 }
